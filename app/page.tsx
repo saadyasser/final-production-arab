fca7b5bc680abce1c9d88bf99f438d404f48399c
saadyasser <saad.wadi.229@gmail.com>
2024-10-22 16:12:24 +0300

    change font style

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Banner from "@/components/Banner";
 
 export default function Home() {
   const startAt = "2022-10-22T12:26:49.668Z";
-  const endAt = "2022-10-28T12:26:49.668Z";
+  const endAt = "2026-10-28T12:26:49.668Z";
 
   return (
     <div>
