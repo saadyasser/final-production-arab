354b15f7b2bcfe3ec6e6fb5e5957bc480563e796
saadyasser <saad.wadi.229@gmail.com>
2024-11-08 16:09:20 +0200

    creating responsive reactive tools section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Banner from "@/components/Banner";
 import "./page.module.css";
+import InteractiveToolsSection from "@/components/InteractiveToolsSection/InteractiveToolsSection";
 
 export default function Home() {
   return (
     <div>
       <Banner startAt="" endAt="" title="" />
+
+      <InteractiveToolsSection />
     </div>
   );
 }
---
+++ components/InteractiveToolsSection/InteractiveToolsSection.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./InteractiveToolsSection.css";
+
+const InteractiveToolsSection = () => {
+  return (
+    <section className="interactive-section">
+      <div className="card">
+        <h2>أدوات تفاعلية والعاب تعليمية</h2>
+        <p className="card-description">
+          تتميز الأكاديمية العربية للبرمجة بتطوير أدوات تفاعلية وألعاب تعليمية
+          مبتكرة باستخدام أساليب الـتلعيب Gamification، مما يجعل عملية التعلم
+          أكثر متعة وجاذبية. نقدم حلولًا مخصصة تلبي احتياجات المؤسسات والشركات،
+          حيث نساعد في تصميم تجارب تعليمية تفاعلية تعزز من تفاعل المستخدمين
+          وتحفزهم على التعلم بطرق غير تقليدية. سواء كنت تبحث عن تطوير مهارات
+          فريق العمل أو تقديم تجربة تعليمية فريدة لعملائك، الأكاديمية العربية
+          للبرمجة هي شريكك المثالي لتحقيق هذه الأهداف بنجاح.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default InteractiveToolsSection;
